95d9cd0882a93c41c673e11a1d5e2a61c03031c6
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-01-21 19:02:43 +0100

    feat: add logic to counter

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,16 +1,18 @@
 import './index.scss';
+import {useState} from "react";
 
 function App() {
-  return (
-      <div className="App">
-        <div>
-          <h2>Счетчик:</h2>
-          <h1>0</h1>
-          <button className="minus">- Минус</button>
-          <button className="plus">Плюс +</button>
+    const [count, setCount] = useState(0)
+    return (
+        <div className="App">
+            <div>
+                <h2>Counter:</h2>
+                <h1>{count}</h1>
+                <button className="minus" onClick={() => setCount(count - 1)}>- Minus</button>
+                <button className="plus" onClick={() => setCount(count + 1)}>Plus +</button>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
