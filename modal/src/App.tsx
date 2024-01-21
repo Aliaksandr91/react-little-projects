d5c999f0b12419aec6c63ca73d71c03d660217c7
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-01-21 19:41:19 +0100

    feat: new component Modal

--- modal/src/App.tsx
+++ modal/src/App.tsx
@@ -1,22 +1,7 @@
-import React, {ReactNode, useState} from 'react';
+import React, {useState} from 'react';
 import './index.scss';
-interface ModalProps {
-    open: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-    children: ReactNode;
-}
-const Modal: React.FC<ModalProps> = ({open, setOpen, children}) => (
-    <div className={`overlay animated ${open ? 'show' : ''}`}>
-        <div className="modal">
-            <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-                <title/>
-                <path
-                    d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
-            </svg>
-            {children}
-        </div>
-    </div>
-)
+import {Modal} from "./Modal";
+
 
 function App() {
     const [open, setOpen] = useState(false)
