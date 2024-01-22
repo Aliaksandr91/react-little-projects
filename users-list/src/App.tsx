3b50d1dd417f7e977ca31117e966835ce4d40921
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-01-22 15:53:06 +0100

    feat: add data to project

--- users-list/src/App.tsx
+++ users-list/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './index.scss';
+import { Users } from './components/Users';
 
-
+// list of users: https://reqres.in/api/users
 
 function App() {
     return (
         <div className="App">
+            <Users />
+            {/* <Success /> */}
         </div>
     );
 }
