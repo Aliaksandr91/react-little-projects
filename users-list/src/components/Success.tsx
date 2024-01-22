111e4c5480df17cb59187f03d83cba2228eef7f9
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-01-22 18:38:00 +0100

    feat: add logic to project

--- users-list/src/components/Success.tsx
+++ users-list/src/components/Success.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-
-export const Success = ({ count }:any) => {
+import React, {FC} from 'react';
+interface SuccessProps {
+    count: number;
+}
+export const Success: FC<SuccessProps> = ({count}) => {
     return (
         <div className="success-block">
-            <img src="/assets/success.svg" alt="Success" />
-            <h3>Успешно!</h3>
-            <p>Всем {count} пользователям отправлено приглашение.</p>
-            <button className="send-invite-btn">Назад</button>
+            <img src="/assets/success.svg" alt="Success"/>
+            <h3>Successful!</h3>
+            <p>{count} users have been sent an invite.</p>
+            <button onClick={()=>window.location.reload()} className="send-invite-btn">Back</button>
         </div>
     );
 };
--- users-list/src/components/Users/index.tsx
+++ users-list/src/components/Users/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {ChangeEvent, FC} from 'react';
 import {Skeleton} from './Skeleton';
-import {User} from "./Users";
+import {User, UserType} from "./User";
 
+interface UsersPropsType {
+    items: UserType[]
+    isLoading: boolean
+    searchValue: string
+    onChangeSearchValue: (value: ChangeEvent<HTMLInputElement>) => void
+    invites: number[]
+    onClickInvite: (id: number) => void
+    onClickSendInvite: () => void
+}
 
-export const Users = ({items, isLoading}) => {
+export const Users: FC<UsersPropsType> = ({
+                                              items,
+                                              isLoading,
+                                              searchValue,
+                                              onChangeSearchValue,
+                                              invites,
+                                              onClickInvite,
+                                              onClickSendInvite
+                                          }) => {
+
     return (
         <>
             <div className="search">
@@ -11,7 +29,8 @@ export const Users = ({items, isLoading}) => {
                     <path
                         d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
                 </svg>
-                <input type="text" placeholder="Найти пользователя..."/>
+                <input value={searchValue} onChange={onChangeSearchValue} type="text"
+                       placeholder="Find a user..."/>
             </div>
             {isLoading ? (
                 <div className="skeleton-list">
@@ -21,10 +40,30 @@ export const Users = ({items, isLoading}) => {
                 </div>
             ) : (
                 <ul className="users-list">
-                    <User/>
+                    {
+                        items
+                            .filter(user => {
+                                const fullName = (user.first_name + user.last_name).toLowerCase()
+                                return fullName.includes(searchValue.toLowerCase()) || user.email.toLowerCase().includes(searchValue.toLowerCase())
+                            })
+                            .map(user => <User
+                                id={user.id}
+                                onClickInvite={onClickInvite}
+                                isInvited={invites.includes(user.id)}
+                                avatar={user.avatar}
+                                email={user.email}
+                                first_name={user.first_name}
+                                last_name={user.last_name}
+                                key={user.id}
+                            />)
+
+                    }
                 </ul>
             )}
-            <button className="send-invite-btn">Отправить приглашение</button>
+            {
+                invites.length > 0 && <button onClick={onClickSendInvite} className="send-invite-btn">Send invite</button>
+            }
+
         </>
     );
 };
