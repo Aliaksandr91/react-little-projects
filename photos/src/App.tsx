1a0361dc27739727bce2e7110559cb4eb77dd50c
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-01-22 21:05:27 +0100

    feat: add logic to project

--- photos/src/App.tsx
+++ photos/src/App.tsx
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
+import {Collection} from "./Collection";
 
+interface CollectionType {
+    category: number
+    name: string
+    photos: string[]
+}
 
+const categories = [
+    {"name": "Все"},
+    {"name": "Море"},
+    {"name": "Горы"},
+    {"name": "Архитектура"},
+    {"name": "Города"}
+]
 
 function App() {
+    const [collections, setCollections] = useState<CollectionType[]>([])
+    const [searchValue, setSearchValue] = useState('')
+    const [categoryId, setCategoryId] = useState(0)
+    const [isLoading, setIsLoading] = useState(true)
+    const [page, setPage] = useState(1)
+    useEffect(() => {
+        setIsLoading(true)
+        const category = categoryId ? `category=${categoryId}` : ''
+
+        fetch(`https://6540fd8045bedb25bfc3032e.mockapi.io/photos?page=${page}&limit=3&${category}`)
+            .then(res => res.json())
+            .then(json => {
+                setCollections(json)
+            })
+            .catch(err => console.log(err))
+            .finally(() => setIsLoading(false))
+    }, [categoryId,page])
+
     return (
         <div className="App">
+            <h1>Моя коллекция фотографий</h1>
+            <div className="top">
+                <ul className="tags">
+                    {
+                        categories.map((category, index) => <li onClick={() => setCategoryId(index)}
+                                                                className={categoryId === index ? 'active' : ''}
+                                                                key={category.name}>{category.name}</li>)
+                    }
+                </ul>
+                <input
+                    value={searchValue}
+                    onChange={(e) => setSearchValue(e.currentTarget.value)}
+                    className="search-input" placeholder="Поиск по названию"/>
+            </div>
+            <div className="content">
+                {
+                    isLoading
+                        ? <h2>Loading...</h2>
+                        : collections
+                            .filter((collection) => {
+                                return collection.name.toLowerCase().includes(searchValue.toLowerCase())
+                            })
+                            .map((collection, index) => (
+                                <Collection
+                                    key={index}
+                                    name={collection.name}
+                                    images={collection.photos}
+                                />)
+                            )
+                }
+            </div>
+            <ul className="pagination">
+                {
+                    [...Array(3)].map((_, i) => <li onClick={() => setPage(i + 1)}
+                                                    className={page === (i + 1) ? 'active' : ''}>{i + 1}</li>)
+                }
+            </ul>
         </div>
     );
 }
